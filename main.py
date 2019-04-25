6c20d865024f2128de9de209a6d9af028c8be8ef
Alexander Shemetovskiy <alexkval@gmail.com>
2019-04-26 00:46:05 +0100

    Increase the NN density to achieve 0.99 accuracy

--- main.py
+++ main.py
@@ -244,7 +244,9 @@ X, y = make_moons(n_samples = N_SAMPLES, noise=0.2, random_state=100)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=42)
 
 # Training
-params_values = train(np.transpose(X_train), np.transpose(y_train.reshape((y_train.shape[0], 1))), nn_architecture, 10000, 0.01)
+epochs = 10000
+learning_rate = 0.1
+params_values = train(np.transpose(X_train), np.transpose(y_train.reshape((y_train.shape[0], 1))), nn_architecture, epochs, learning_rate)
 
 # Prediction
 Y_test_hat, _ = full_forward_propagation(np.transpose(X_test), params_values, nn_architecture)
